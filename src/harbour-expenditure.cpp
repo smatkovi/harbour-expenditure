618c780be51b7d123fb5c00804546888c1a1f4b5
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2024-09-06 14:10:29 +0200

    Enable Opal modules

--- src/harbour-expenditure.cpp
+++ src/harbour-expenditure.cpp
@@ -1,25 +1,31 @@
-#ifdef QT_QML_DEBUG
+/*
+ * This file is part of harbour-expenditure.
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * SPDX-FileCopyrightText: 2022 Tobias Planitzer
+ * SPDX-FileCopyrightText: 2023-2024 Mirian Margiani
+ */
+
 #include <QtQuick>
-#endif
-
+#include <QDebug>
 #include <sailfishapp.h>
+#include "requires_defines.h"
 
-#include <QtQuick> // FileIO
 #include "File.h" // FileIO
 
 int main(int argc, char *argv[])
 {
-    // SailfishApp::main() will display "qml/harbour-expenditure.qml", if you need more
-    // control over initialization, you can use:
-    //
-    //   - SailfishApp::application(int, char *[]) to get the QGuiApplication *
-    //   - SailfishApp::createView() to get a new QQuickView * instance
-    //   - SailfishApp::pathTo(QString) to get a QUrl to a resource file
-    //   - SailfishApp::pathToMainQml() to get a QUrl to the main QML file
-    //
-    // To display the view, call "show()" (will show fullscreen on device).
-
     qmlRegisterType<File>("FileIO", 1, 0, "TextFileIO"); // FileIO
 
-    return SailfishApp::main(argc, argv);
+    QScopedPointer<QGuiApplication> app(SailfishApp::application(argc, argv));
+    app->setOrganizationName("org.tplabs"); // needed for Sailjail
+    app->setApplicationName("expenditure");
+
+    QScopedPointer<QQuickView> view(SailfishApp::createView());
+    view->rootContext()->setContextProperty("APP_VERSION", QString(APP_VERSION));
+    view->rootContext()->setContextProperty("APP_RELEASE", QString(APP_RELEASE));
+
+    view->engine()->addImportPath(SailfishApp::pathTo("qml/modules").toString());
+    view->setSource(SailfishApp::pathToMainQml());
+    view->show();
+    return app->exec();
 }
